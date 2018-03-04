905e117f22a8e042364fc0801654eb0cfaa4b9f9
Pranay Patodi <pranay343@gmail.com>
2018-03-05 01:13:38 +0530

    new

--- nearby_places/views.py
+++ nearby_places/views.py
@@ -48,7 +48,7 @@ def Cafe(request):
 			context = { 'z' : z ,
 						  
 				}
-                        return render(request,'nearbyplaces.html',context)
+			return render(request,'nearbyplaces.html',context)
 	else :
 		return redirect('login')
 		
@@ -92,178 +92,178 @@ def Hospital(request):
 			context = { 'z' : z   }
 			return render(request,'nearbyplaces.html',context)
 		else :
-                    return redirect('login')	
+		    return redirect('login')	
 			
 		
 def School(request):
     if request.user.is_authenticated():		
-        url = 'http://freegeoip.net/json/'
-        with closing(urlopen(url)) as response:
-                location = json.loads(response.read())
-                lng =  location['longitude']
-                lat =  location['latitude']
+	url = 'http://freegeoip.net/json/'
+	with closing(urlopen(url)) as response:
+		location = json.loads(response.read())
+		lng =  location['longitude']
+		lat =  location['latitude']
 		lat1 = decimal.Decimal(lat)+decimal.Decimal(0.007798)
 		lng1 = decimal.Decimal(lng)+decimal.Decimal(0.044059)		
 
-        name = explore.objects.all()
-        AUTH_KEY = 'AIzaSyAF1M_y5ABZdZRWKTkhMjMJuq5Ysz6swm8'
-        LOCATION = str(lat1) + "," + str(lng1)
-        RADIUS = 1000
-        TYPES = 'school'
-        MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
-                                           '?location=%s'
-                                           '&radius=%s'
-                                           '&types=%s'
-                                           '&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
-        f = urllib2.urlopen(MyUrl)
-        json_string = f.read()
-        f.close()
-        location = json.loads(json_string)
+	name = explore.objects.all()
+	AUTH_KEY = 'AIzaSyAF1M_y5ABZdZRWKTkhMjMJuq5Ysz6swm8'
+	LOCATION = str(lat1) + "," + str(lng1)
+	RADIUS = 1000
+	TYPES = 'school'
+	MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+					   '?location=%s'
+					   '&radius=%s'
+					   '&types=%s'
+					   '&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
+	f = urllib2.urlopen(MyUrl)
+	json_string = f.read()
+	f.close()
+	location = json.loads(json_string)
 
-        for r in location['results']:
-            
-            if explore.objects.filter(name=r['name']) :
-                z='noo'
-            #elif r['rating'] is not None :    
-             #   explore.objects.create(name=r['name'],rating=r['rating'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='cafe')
-             #   z = 'uyoo'
-            else:
-                explore.objects.create(name=r['name'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='school')    
+	for r in location['results']:
+	    
+	    if explore.objects.filter(name=r['name']) :
+		z='noo'
+	    #elif r['rating'] is not None :    
+	     #   explore.objects.create(name=r['name'],rating=r['rating'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='cafe')
+	     #   z = 'uyoo'
+	    else:
+		explore.objects.create(name=r['name'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='school')    
 
-        z = explore.objects.filter(type='school')
-        context = { 'z' : z   }
-        return render(request,'nearbyplaces.html',context)
+	z = explore.objects.filter(type='school')
+	context = { 'z' : z   }
+	return render(request,'nearbyplaces.html',context)
     else :
 		return redirect('login')	
 		
 	
 def Shopping_mall(request):
     if request.user.is_authenticated():		
-        url = 'http://freegeoip.net/json/'
-        with closing(urlopen(url)) as response:
-                location = json.loads(response.read())
-                lng =  location['longitude']
-                lat =  location['latitude']
+	url = 'http://freegeoip.net/json/'
+	with closing(urlopen(url)) as response:
+		location = json.loads(response.read())
+		lng =  location['longitude']
+		lat =  location['latitude']
 		lat1 = decimal.Decimal(lat)+decimal.Decimal(0.007798)
 		lng1 = decimal.Decimal(lng)+decimal.Decimal(0.044059)		
 
-        name = explore.objects.all()
-        AUTH_KEY = 'AIzaSyAF1M_y5ABZdZRWKTkhMjMJuq5Ysz6swm8'
-        LOCATION = str(lat1) + "," + str(lng1)
-        RADIUS = 1000
-        TYPES = 'shopping_mall'
-        MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
-                                           '?location=%s'
-                                           '&radius=%s'
-                                           '&types=%s'
-                                           '&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
-        f = urllib2.urlopen(MyUrl)
-        json_string = f.read()
-        f.close()
-        location = json.loads(json_string)
+	name = explore.objects.all()
+	AUTH_KEY = 'AIzaSyAF1M_y5ABZdZRWKTkhMjMJuq5Ysz6swm8'
+	LOCATION = str(lat1) + "," + str(lng1)
+	RADIUS = 1000
+	TYPES = 'shopping_mall'
+	MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+					   '?location=%s'
+					   '&radius=%s'
+					   '&types=%s'
+					   '&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
+	f = urllib2.urlopen(MyUrl)
+	json_string = f.read()
+	f.close()
+	location = json.loads(json_string)
 
-        for r in location['results']:
-            
-            if explore.objects.filter(name=r['name']) :
-                z='noo'
-            #elif r['rating'] is not None :    
-             #   explore.objects.create(name=r['name'],rating=r['rating'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='cafe')
-             #   z = 'uyoo'
-            else:
-                explore.objects.create(name=r['name'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='shopping_mall')    
+	for r in location['results']:
+	    
+	    if explore.objects.filter(name=r['name']) :
+		z='noo'
+	    #elif r['rating'] is not None :    
+	     #   explore.objects.create(name=r['name'],rating=r['rating'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='cafe')
+	     #   z = 'uyoo'
+	    else:
+		explore.objects.create(name=r['name'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='shopping_mall')    
 
-        z = explore.objects.filter(type='shopping_mall')
-        context = { 'z' : z   }
-        return render(request,'nearbyplaces.html',context)
+	z = explore.objects.filter(type='shopping_mall')
+	context = { 'z' : z   }
+	return render(request,'nearbyplaces.html',context)
     else :
 		return redirect('login')	
 		
 	
 def Restaurant(request):
     if request.user.is_authenticated():		
-        url = 'http://freegeoip.net/json/'
-        with closing(urlopen(url)) as response:
-                location = json.loads(response.read())
-                lng =  location['longitude']
-                lat =  location['latitude']
+	url = 'http://freegeoip.net/json/'
+	with closing(urlopen(url)) as response:
+		location = json.loads(response.read())
+		lng =  location['longitude']
+		lat =  location['latitude']
 		lat1 = decimal.Decimal(lat)+decimal.Decimal(0.007798)
 		lng1 = decimal.Decimal(lng)+decimal.Decimal(0.044059)		
 
-        name = explore.objects.all()
-        AUTH_KEY = 'AIzaSyAF1M_y5ABZdZRWKTkhMjMJuq5Ysz6swm8'
-        LOCATION = str(lat1) + "," + str(lng1)
-        RADIUS = 1000
-        TYPES = 'restaurant'
-        MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
-                                           '?location=%s'
-                                           '&radius=%s'
-                                           '&types=%s'
-                                           '&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
-        f = urllib2.urlopen(MyUrl)
-        json_string = f.read()
-        f.close()
-        location = json.loads(json_string)
+	name = explore.objects.all()
+	AUTH_KEY = 'AIzaSyAF1M_y5ABZdZRWKTkhMjMJuq5Ysz6swm8'
+	LOCATION = str(lat1) + "," + str(lng1)
+	RADIUS = 1000
+	TYPES = 'restaurant'
+	MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+					   '?location=%s'
+					   '&radius=%s'
+					   '&types=%s'
+					   '&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
+	f = urllib2.urlopen(MyUrl)
+	json_string = f.read()
+	f.close()
+	location = json.loads(json_string)
 
-        for r in location['results']:
-            
-            if explore.objects.filter(name=r['name']) :
-                z='noo'
-            #elif r['rating'] is not None :    
-             #   explore.objects.create(name=r['name'],rating=r['rating'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='cafe')
-             #   z = 'uyoo'
-            else:
-                explore.objects.create(name=r['name'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='restaurant')    
+	for r in location['results']:
+	    
+	    if explore.objects.filter(name=r['name']) :
+		z='noo'
+	    #elif r['rating'] is not None :    
+	     #   explore.objects.create(name=r['name'],rating=r['rating'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='cafe')
+	     #   z = 'uyoo'
+	    else:
+		explore.objects.create(name=r['name'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='restaurant')    
 
-        z = explore.objects.filter(type='restaurant')
-        context = { 'z' : z   }
-        return render(request,'nearbyplaces.html',context)
+	z = explore.objects.filter(type='restaurant')
+	context = { 'z' : z   }
+	return render(request,'nearbyplaces.html',context)
     else :
 		return redirect('login')	
 			
 	
 def Movie_theater(request):
     if request.user.is_authenticated():		
-        url = 'http://freegeoip.net/json/'
-        with closing(urlopen(url)) as response:
-                location = json.loads(response.read())
-                lng =  location['longitude']
-                lat =  location['latitude']
+	url = 'http://freegeoip.net/json/'
+	with closing(urlopen(url)) as response:
+		location = json.loads(response.read())
+		lng =  location['longitude']
+		lat =  location['latitude']
 		lat1 = decimal.Decimal(lat)+decimal.Decimal(0.007798)
 		lng1 = decimal.Decimal(lng)+decimal.Decimal(0.044059)		
 
-        name = explore.objects.all()
-        AUTH_KEY = 'AIzaSyAF1M_y5ABZdZRWKTkhMjMJuq5Ysz6swm8'
-        LOCATION = str(lat1) + "," + str(lng1)
-        RADIUS = 1000
-        TYPES = 'movie_theater'
-        MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
-                                           '?location=%s'
-                                           '&radius=%s'
-                                           '&types=%s'
-                                           '&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
-        f = urllib2.urlopen(MyUrl)
-        json_string = f.read()
-        f.close()
-        location = json.loads(json_string)
+	name = explore.objects.all()
+	AUTH_KEY = 'AIzaSyAF1M_y5ABZdZRWKTkhMjMJuq5Ysz6swm8'
+	LOCATION = str(lat1) + "," + str(lng1)
+	RADIUS = 1000
+	TYPES = 'movie_theater'
+	MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+					   '?location=%s'
+					   '&radius=%s'
+					   '&types=%s'
+					   '&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
+	f = urllib2.urlopen(MyUrl)
+	json_string = f.read()
+	f.close()
+	location = json.loads(json_string)
 
-        for r in location['results']:
-            
-            if explore.objects.filter(name=r['name']) :
-                z='noo'
-            #elif r['rating'] is not None :    
-             #   explore.objects.create(name=r['name'],rating=r['rating'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='cafe')
-             #   z = 'uyoo'
-            else:
-                explore.objects.create(name=r['name'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='movie_theater')    
+	for r in location['results']:
+	    
+	    if explore.objects.filter(name=r['name']) :
+		z='noo'
+	    #elif r['rating'] is not None :    
+	     #   explore.objects.create(name=r['name'],rating=r['rating'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='cafe')
+	     #   z = 'uyoo'
+	    else:
+		explore.objects.create(name=r['name'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='movie_theater')    
 
-        z = explore.objects.filter(type='movie_theater')
-        context = { 'z' : z   }
-        return render(request,'nearbyplaces.html',context)
+	z = explore.objects.filter(type='movie_theater')
+	context = { 'z' : z   }
+	return render(request,'nearbyplaces.html',context)
     else :
 		return redirect('login')	
 		
 def detail(request,id) : 
-                  z = explore.objects.get(pk = id)
+		  z = explore.objects.get(pk = id)
 		  context = { 'z' : z ,
 					}
 		  return render(request,'detail.html',context)		
