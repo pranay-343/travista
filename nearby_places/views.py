8a3c7c87ddad09d4de43130a8ddd64b33295c6b3
Pranay Patodi <pranay343@gmail.com>
2018-03-05 13:39:31 +0530

    new

--- nearby_places/views.py
+++ nearby_places/views.py
@@ -139,11 +139,11 @@ def Shopping_mall(request):
     if request.user.is_authenticated():         
       url = 'http://freegeoip.net/json/'
       with closing(urlopen(url)) as response:
-        location = json.loads(response.read())
-	lng = location['longitude']
-	lat = location['latitude']
-	lat1 = decimal.Decimal(lat)+decimal.Decimal(0.007798)
-	lng1 = decimal.Decimal(lng)+decimal.Decimal(0.044059)           
+       location = json.loads(response.read())
+       lng = location['longitude']
+       lat = location['latitude']
+       lat1 = decimal.Decimal(lat)+decimal.Decimal(0.007798)
+       lng1 = decimal.Decimal(lng)+decimal.Decimal(0.044059)           
 
       name = explore.objects.all()
       AUTH_KEY = 'AIzaSyAF1M_y5ABZdZRWKTkhMjMJuq5Ysz6swm8'
