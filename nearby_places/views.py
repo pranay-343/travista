e5ed1bd9dbeed96a0cfddd2dd856b3dd6fabc609
Pranay Patodi <pranay343@gmail.com>
2018-03-05 13:34:46 +0530

    new

--- nearby_places/views.py
+++ nearby_places/views.py
@@ -137,44 +137,44 @@ def School(request):
 	
 def Shopping_mall(request):
     if request.user.is_authenticated():         
-	url = 'http://freegeoip.net/json/'
-	with closing(urlopen(url)) as response:
-		location = json.loads(response.read())
-		lng =  location['longitude']
-		lat =  location['latitude']
-		lat1 = decimal.Decimal(lat)+decimal.Decimal(0.007798)
-		lng1 = decimal.Decimal(lng)+decimal.Decimal(0.044059)           
+      url = 'http://freegeoip.net/json/'
+      with closing(urlopen(url)) as response:
+        location = json.loads(response.read())
+	lng =  location['longitude']
+	lat =  location['latitude']
+	lat1 = decimal.Decimal(lat)+decimal.Decimal(0.007798)
+	lng1 = decimal.Decimal(lng)+decimal.Decimal(0.044059)           
 
-	name = explore.objects.all()
-	AUTH_KEY = 'AIzaSyAF1M_y5ABZdZRWKTkhMjMJuq5Ysz6swm8'
-	LOCATION = str(lat1) + "," + str(lng1)
-	RADIUS = 1000
-	TYPES = 'shopping_mall'
-	MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+      name = explore.objects.all()
+      AUTH_KEY = 'AIzaSyAF1M_y5ABZdZRWKTkhMjMJuq5Ysz6swm8'
+      LOCATION = str(lat1) + "," + str(lng1)
+      RADIUS = 1000
+      TYPES = 'shopping_mall'
+      MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
 					   '?location=%s'
 					   '&radius=%s'
 					   '&types=%s'
 					   '&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
-	f = urllib2.urlopen(MyUrl)
-	json_string = f.read()
-	f.close()
-	location = json.loads(json_string)
+      f = urllib2.urlopen(MyUrl)
+      json_string = f.read()
+      f.close()
+      location = json.loads(json_string)
 
-	for r in location['results']:
-	    
-	    if explore.objects.filter(name=r['name']) :
-		z='noo'
+      for r in location['results']:
+  
+        if explore.objects.filter(name=r['name']) :
+	  z='noo'
 	    #elif r['rating'] is not None :    
 	     #   explore.objects.create(name=r['name'],rating=r['rating'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='cafe')
 	     #   z = 'uyoo'
-	    else:
-		explore.objects.create(name=r['name'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='shopping_mall')    
+	else:
+	 explore.objects.create(name=r['name'],lat=r['geometry']['location']['lat'],lng=r['geometry']['location']['lng'],vicinity=r['vicinity'],type='shopping_mall')    
 
 	z = explore.objects.filter(type='shopping_mall')
 	context = { 'z' : z   }
 	return render(request,'nearbyplaces.html',context)
     else :
-		return redirect('login')        
+	return redirect('login')        
 		
 	
 def Restaurant(request):
